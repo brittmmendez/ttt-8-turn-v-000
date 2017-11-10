e39c77d27ce24ceae34f8316de374a781da37cea
Brittany Morris <brittmmendez@gmail.com>
2017-11-10 15:43:51 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,6 +5,9 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+def turn(board)
+  puts "Please enter 1-9:"
+end
 
 def input_to_index(move)
 index=(move.to_i)-1
