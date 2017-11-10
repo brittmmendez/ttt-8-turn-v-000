d4eb73361da754c20fe3335f62ef579d3c6bf856
Brittany Morris <brittmmendez@gmail.com>
2017-11-10 17:01:06 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -24,15 +24,7 @@ index=(move.to_i)-1
 end
 
 def valid_move?(board,index)
-  if (index.between?(0, 8)) && position_taken?(board,index)
-    true
-  else
-    false
-  end
-end
-
-def position_taken?(board,index)
-  if (board[index]==" "||board[index]==""||board[index]==nil)
+  if (index.between?(0, 8)) && ((board[index]==" "||board[index]==""||board[index]==nil))
     true
   else
     false
