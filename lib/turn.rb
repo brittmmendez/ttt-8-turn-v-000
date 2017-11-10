23cb8fc8a23e811a4500256506c8ef8788fc0417
Brittany Morris <brittmmendez@gmail.com>
2017-11-10 15:36:57 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,6 +17,13 @@ def valid_move?(board,index)
         false
   end
 end
+def position_taken?(board,index)
+  if board[index]==" "||board[index]==""||board[index]==nil
+    true
+  else
+  false
+end
+end
 
 def move(array,index,value="X")
   array[index]=value
