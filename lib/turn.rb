2b49f1e15484426f6b59c9716a8cf7d11483fef9
Brittany Morris <brittmmendez@gmail.com>
2017-11-10 15:57:53 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,15 +1,16 @@
 def turn(board)
   puts "Please enter 1-9:"
 end
+def input_to_index(move)
+index=(move.to_i)-1
+end
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-def input_to_index(move)
-index=(move.to_i)-1
 end
 def valid_move?(board,index)
   if (index.between?(0, 8)) && (board[index]==" "||board[index]==""||board[index]==nil)
